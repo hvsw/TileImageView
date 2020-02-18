452eb187f377fa9910f166e4432785cb65ea5e64
Henrique Valcanaia <henriqueindalencio@gmail.com>
2020-02-18 16:58:14 -0300

    Initial Commit

---
+++ GhostedCellList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GhostedCellList
+//
+//  Created by Henrique Valcanaia on 2020-02-18.
+//  Copyright © 2020 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
